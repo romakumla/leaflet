b34de8281948c8b44038e37034e6715ec90b9547
romakumla <romankumladze@gmail.com>
2020-09-12 14:15:09 +0400

    firs eddit

---
+++ main.js
@@ -0,0 +1,8 @@
+var map = L.map("map").setView([27.2, 83.95], 10);
+
+var osm = L.tileLayer("https://{S}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    "&copy; <a href='https://openstreetmap.org/copyright'> Openstreet map</a> contributors",
+});
+
+osm.addTo(map);
